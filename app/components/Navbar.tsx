74d857ec2a08fb46db7b64dd663be6c68c47707f
Husen <husenikary@mail.ru>
2025-01-07 20:31:26 +0500

    Navbar added assets update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+import logo from "../../public/logo.png"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+
+            <Link href="/">
+                <Image src={logo} alt="logo" width={144} height={30}/>
+            </Link>
+
+            <div className="flex items-center gap-5">
+
+            </div>
+
+        </nav>
+    </header>
   )
 }
 

06282c6bccd27f657cd0572b0acf3d7acc24ba23
Husen <husenikary@mail.ru>
2025-01-10 22:57:22 +0500

    Public folder create

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="forn-work-sans">
+            <Navbar />
             {children}
         </main>
     )

5663679555bb62ed227a09425d7897826d986b22
Husen <husenikary@mail.ru>
2025-01-10 22:53:06 +0500

    Login code ready

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,8 +12,38 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
         <nav className="flex justify-between items-center">
 
-            
+          <Link href='/'>
+            <Image src={logo} alt="logo" width={144} height={30} />
+          </Link>
 
+          <div className="flex items-center gap-5 text-black">
+
+          {session && session?.user ? (
+
+            <>
+            <Link href='/startup/create'>
+              <span>Create</span>
+            </Link>
+
+            <button onClick={signOut}>
+                <span>Logout</span>
+            </button>
+
+            <Link href={`/user/${session?.id}`}>
+              <span>{session?.user?.name}</span>
+            </Link>
+            </>
+
+            ) : (
+              <>
+               <button onClick={signIn('github')}>
+                <span>Login</span>
+               </button>
+              </>
+            )}
+
+
+          </div>
         </nav>
     </header>
   )

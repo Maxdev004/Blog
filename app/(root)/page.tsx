c401c8a73e15a0ec88567b35c2c28a7a7ce25430
Husen <husenikary@mail.ru>
2025-01-14 09:11:36 +0500

    Form Component Add

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import React, { memo } from 'react'
+import SearchForm from '../components/SearchForm'
 
 const Home = () => {
   return (
-    <div className='heading'>Home</div>
+    <>
+    <section className='pink_container'>
+
+      <div className='heading'>Pitch Your Startup, <br/>Connect With Entrepreneurs </div>
+
+      <p className='sub-heading !max-w-3xl'>
+        Submit Ideas, Vote on Pitches and Get Noticed in Virtual
+      </p>
+
+      <SearchForm />
+
+    </section>
+    </>
   )
 }
 

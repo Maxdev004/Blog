599f321bd1e3f3d163465a48bfa849538f4869b8
Husen <husenikary@mail.ru>
2025-01-15 12:47:56 +0500

    Card added

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-// export function parseServerActionResponse<T>(response: T) {
-//     return JSON.parse(JSON.stringify(response));
-// }
+export function parseServerActionResponse<T>(response: T) {
+    return JSON.parse(JSON.stringify(response));
+}
 
 export function formatDate(date: string) {
     return new Date(date).toLocaleDateString("en-US", {
@@ -17,12 +17,12 @@ export function formatDate(date: string) {
     });
 }
 
-// export function formatNumber(number: number) {
-//     if (number >= 1000000) {
-//         return (number / 1000000).toFixed(1).replace(/\.0$/, "") + "M"; // Convert to millions
-//     } else if (number >= 1000) {
-//         return (number / 1000).toFixed(1).replace(/\.0$/, "") + "k"; // Convert to thousands
-//     } else {
-//         return number.toString(); // Return the number as is if below 1000
-//     }
-// }
+export function formatNumber(number: number) {
+    if (number >= 1000000) {
+        return (number / 1000000).toFixed(1).replace(/\.0$/, "") + "M"; // Convert to millions
+    } else if (number >= 1000) {
+        return (number / 1000).toFixed(1).replace(/\.0$/, "") + "k"; // Convert to thousands
+    } else {
+        return number.toString(); // Return the number as is if below 1000
+    }
+}

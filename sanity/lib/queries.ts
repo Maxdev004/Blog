aac5a502be8919489a2dca67493dd2fdd2f5df86
Husen <husenikary@mail.ru>
2025-01-19 20:23:55 +0500

    query fix

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,13 +1,13 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUOS_QUERY = defineQuery(`*[_type == 'startup' && defined(slug.current)] | order(_createdAt, desc) {
+export const STARTUPS_QUERY = defineQuery(`*[_type == 'startup' && defined(slug.current)] | order(_createdAt, desc) {
   _id, 
   title, 
   slug, 
   _createdAt, 
   author -> {
     _id, name, image, bio
-  } 
+  },
   views, 
   description,  
   category, 

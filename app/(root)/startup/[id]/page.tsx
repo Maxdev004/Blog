ae998f4416e07a553fdca98c961d510ddb315ff6
Husen <husenikary@mail.ru>
2025-01-22 22:20:47 +0500

    update node js

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { formatDate } from '@/app/lib/utils'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
@@ -6,15 +8,14 @@ import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import markdownit from 'markdown-it'
 
+const md = markdownit()
+
 const page = async ({ params }: {params: Promise<{ id: string }>}) => {
     const id = (await params).id
 
     const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
 
     if(!post) return notFound()
-    
-    const md = markdownit()
-    const parsedContent = md.render(post?.pitch || '')
     
 
   return <>
@@ -41,14 +42,14 @@ const page = async ({ params }: {params: Promise<{ id: string }>}) => {
           <p className='category-tag'>{post.category}</p>
         </div>
         <h3 className='text-30-bold'>Pitch Details</h3>
-        {parsedContent ? (
+        {/* {parsedContent ? (
           <article
-          className='prose max-w-4xl fornt-wora'
+          className='prose max-w-4xl font-work-sans break-all'
           dangerouslySetInnerHTML={{ __html: parsedContent }}
           />
         ) : (
           <p className='no-result'>No details provided</p>
-        )}
+        )} */}
       </div>
     </section>
 

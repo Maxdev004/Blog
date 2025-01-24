7b3fb7b49d7e95e46088bd2d0d161c8b73a1ec69
Husen <husenikary@mail.ru>
2025-01-24 18:39:25 +0500

    Pink container added

--- components/View.tsx
+++ components/View.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Ping from './Ping'
 
-const View = () => {
+const View = ({ id }: { id: number}) => {
   return (
-    <div>View</div>
+    <div className="view-container">
+        <div className="absolute -top-2 -right-2">
+            <Ping />
+        </div>
+
+        <p className='view-text'>
+            <span className='font-black'>100 views</span>
+        </p>
+    </div>
   )
 }
 

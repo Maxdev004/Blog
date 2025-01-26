6c151e4c98f967ec78bd99a36b08ee9d0e569cf5
Husen <husenikary@mail.ru>
2025-01-26 11:23:50 +0500

    create page form added

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,8 +1,26 @@
+"use client"
 
+import { useState } from "react"
+import { Input } from "./ui/input"
 
 const StartupForm = () => {
+
+    const [errors, setErrors] = useState({})
+
   return (
-    <div>StartupForm</div>
+    <form action={() => {}} className="startup-form">
+        <div>
+            <label className="startup-form-title" htmlFor="title">TITLE</label>
+            <Input
+            id="title"
+            name="title"
+            className="startup-form_input"
+            required
+            placeholder="Startup Title"
+            />
+            {errors.title && <p className="startup-form_error">{errors.title}</p>}
+        </div>
+    </form>
   )
 }
 

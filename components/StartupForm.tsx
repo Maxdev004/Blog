f3255f288f349156fe325c61568ba7eee7ffd1c9
Husen <husenikary@mail.ru>
2025-01-26 11:27:46 +0500

    more input fields

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -5,12 +5,36 @@ import { Input } from "./ui/input"
 
 const StartupForm = () => {
 
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState<Record<string, string>>({})
 
   return (
     <form action={() => {}} className="startup-form">
         <div>
-            <label className="startup-form-title" htmlFor="title">TITLE</label>
+            <label className="startup-form_label" htmlFor="title">TITLE</label>
+            <Input
+            id="title"
+            name="title"
+            className="startup-form_input"
+            required
+            placeholder="Startup Title"
+            />
+            {errors.title && <p className="startup-form_error">{errors.title}</p>}
+        </div>
+
+        <div>
+            <label className="startup-form_label" htmlFor="title">TITLE</label>
+            <Input
+            id="title"
+            name="title"
+            className="startup-form_input"
+            required
+            placeholder="Startup Title"
+            />
+            {errors.title && <p className="startup-form_error">{errors.title}</p>}
+        </div>
+
+        <div>
+            <label className="startup-form_label" htmlFor="title">TITLE</label>
             <Input
             id="title"
             name="title"

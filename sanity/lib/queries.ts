d8d9e023efb49664d885c8d3ff5d43708c2c4e8b
Ikary <husenikary@mail.ru>
2025-01-27 20:19:25 +0500

    Update queries.ts

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`*[_type == 'startup' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search ] | order(_createdAt, desc) {

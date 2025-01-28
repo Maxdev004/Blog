5b3dd94db3dc76a1d083836737cf9c715d63296e
Husen <husenikary@mail.ru>
2025-01-28 20:46:27 +0500

    parseServerActionResponse use

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,14 @@
 "use server"
 
 import { auth } from "@/auth"
+import { parseServerActionResponse } from "./utils"
 
 export const crearePitch = async ( state: any, form: FormData, pitch: string) => {
     const session = await auth()
+
+    if(!session) return parseServerActionResponse({ error: "Not signed in",  status: "ERROR"})
+
+    
 }
 
 export default crearePitch

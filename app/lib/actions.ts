c7a16efdf71a152c6e71c05bdab9bb3e60975aba
Husen <husenikary@mail.ru>
2025-01-28 20:56:33 +0500

    data patch added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth"
 import { parseServerActionResponse } from "./utils"
+import slugify from "slugify"
 
 export const crearePitch = async ( state: any, form: FormData, pitch: string) => {
     const session = await auth()
@@ -11,6 +12,31 @@ export const crearePitch = async ( state: any, form: FormData, pitch: string) =>
     const { title, description, category, link } = Object.fromEntries(
         Array.from(form).filter(([key]) => key !== "pitch")
     )
+
+    const slug = slugify(title as string, { lower: true, strict: true });
+
+    try {
+        const startup = {
+            title,
+            description,
+            category,
+            link: link,
+            slug: {
+                _type: slug,
+                current: slug,
+            },
+            author: {
+                _type: "reference",
+                ref: session.id,
+            },
+            pitch,
+        }
+
+    } catch(error) {
+        console.log(error)
+
+        return parseServerActionResponse({ error: JSON.stringify(error), status: "ERROR"})
+    }
 }
 
 export default crearePitch

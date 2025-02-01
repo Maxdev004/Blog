bb166a6281421847977d6acf2a8bbb3615edbc3e
Husen <husenikary@mail.ru>
2025-02-01 10:01:55 +0500

    data fetch

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,12 +1,20 @@
 import { auth } from "@/auth"
+import { client } from "@/sanity/lib/client"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
+import { notFound } from "next/navigation"
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
     const id = (await params).id
     const session = auth()
-    
+
+    const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id })
+    if(!user) return notFound()
+
   return (
-    <div>page</div>
+    <div>
+        Page
+    </div>
   )
 }
 
